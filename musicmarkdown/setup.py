4780ce71015361c023f2875bb47a32bd557743c2
Marco - laptop <marco@marco.me>
2019-04-09 19:02:55 +0200

    upates for PyPl

--- musicmarkdown/setup.py
+++ musicmarkdown/setup.py
@@ -1,14 +1,22 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
 setup(
     name='musicmarkdown',
     version='0.1',
     description='Makrdown compiler for music grids and structures',
+    long_description=long_description,
     author='Marco Pascucci',
     author_email='marpas.paris@gmail.com',
     url='https://github.com/mpascucci/music_grid_markdown',
     packages=find_packages(),
     package_data={'musicmd': ['LICENCE.txt', 'resources/*']},
     include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
     # install_requires=[""]
 )
