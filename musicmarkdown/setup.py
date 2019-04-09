b661cdf7943208f703db48765949cb4f0bf11039
Marco - laptop <marco@marco.me>
2019-04-09 14:54:58 +0200

    modified setup.py

--- musicmarkdown/setup.py
+++ musicmarkdown/setup.py
@@ -2,8 +2,13 @@ from setuptools import setup, find_packages
 
 setup(
     name='musicmarkdown',
+    version='0.1',
+    description='Makrdown compiler for music grids and structures',
+    author='Marco Pascucci',
+    author_email='marpas.paris@gmail.com',
+    url='https://github.com/mpascucci/music_grid_markdown',
     packages=find_packages(),
-    package_data={'musicmd': ['resources/*']},
+    package_data={'musicmd': ['LICENCE.txt', 'resources/*']},
     include_package_data=True,
     # install_requires=[""]
 )
