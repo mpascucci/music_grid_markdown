e8c9f85b2a21e731163fdc93171d7eb99455233e
Marco Pascucci <36882726+mpascucci@users.noreply.github.com>
2019-04-09 08:19:07 +0200

    Update setup.py

--- musicmarkdown/setup.py
+++ musicmarkdown/setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='musicmarkdown',
     packages=find_packages(),
+    package_data={'musicmd': ['resources/*']},
     include_package_data=True,
     # install_requires=[""]
 )
